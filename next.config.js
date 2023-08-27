42886dc6dc4a3362211f112450a022f0a26d3af2
Stratos Ntallaris <stratos@Stratos>
2023-08-28 00:14:06 +0300

    added generateEtags in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/sports',
-          permanent: false,
-        },
-      ];
-    },
-  };
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sports',
+        permanent: false,
+      },
+    ]
+  },
+  generateEtags: false,
+}
 
 module.exports = nextConfig
