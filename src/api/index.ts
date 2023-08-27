f8e84f8620ee7e9f46f837cdd02101cc9ee64593
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2023-08-27 19:21:01 +0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,15 +7,16 @@ var requestOptions: any = {
   cache: "no-store",
 };
 
+var API_URL = process.env.API_URL!;
 export const getSportsOdds = (sport: string) =>
-  fetch(`http://3.68.65.235:3000/odds/${sport}/live`, requestOptions)
+  fetch(`http://${API_URL}/odds/${sport}/live`, requestOptions)
     .then((response) => response.text())
     .then((result) => JSON.parse(result))
     .catch((error) => console.log("error", error));
 
 export const getOddsGroupedByLeauge = (sport: string) =>
   fetch(
-    `http://3.68.65.235:3000/odds/${sport}/live/groupBy/league`,
+    `http://${API_URL}/odds/${sport}/live/groupBy/league`,
     requestOptions
   )
     .then((response) => response.text())
@@ -23,37 +24,37 @@ export const getOddsGroupedByLeauge = (sport: string) =>
     .catch((error) => console.log("error", error));
 
 export const getPregameNames = () =>
-  fetch(`http://3.68.65.235:3000/odds/sports/pregame/names`, requestOptions)
+  fetch(`http://${API_URL}/odds/sports/pregame/names`, requestOptions)
     .then((response) => response.text())
     .then((result) => JSON.parse(result))
     .catch((error) => console.log("error", error));
 
 
 export const getPregameLeagues = (sport: string) =>
-  fetch(`http://3.68.65.235:3000/odds/${sport}/pregame/leagues`, requestOptions)
+  fetch(`http://${API_URL}/odds/${sport}/pregame/leagues`, requestOptions)
     .then((response) => response.text())
     .then((result) => JSON.parse(result))
     .catch((error) => console.log("error", error));
 
 export const getPregameSoccerEngland = () =>
-  fetch(`http://3.68.65.235:3000/odds/soccer/pregame/events/England: Fa Community Shield`, requestOptions)
+  fetch(`http://${API_URL}/odds/soccer/pregame/events/England: Fa Community Shield`, requestOptions)
     .then((response) => response.text())
     .then((result) => JSON.parse(result))
     .catch((error) => console.log("error", error));
 
 export const getPregameSoccer = (leagues: string) =>
-  fetch(`http://3.68.65.235:3000/odds/soccer/pregame/events/${leagues}`, requestOptions)
+  fetch(`http://${API_URL}/odds/soccer/pregame/events/${leagues}`, requestOptions)
     .then((response) => response.text())
     .then((result) => JSON.parse(result))
     .catch((error) => console.log("error", error));
 
 export const getPregame = (sport:string, leagues: string) =>
-  fetch(`http://3.68.65.235:3000/odds/${sport}/pregame/events/${leagues}`, requestOptions)
+  fetch(`http://${API_URL}/odds/${sport}/pregame/events/${leagues}`, requestOptions)
     .then((response) => response.text())
     .then((result) => JSON.parse(result))
     .catch((error) => console.log("error", error));
 export const getPregamesLeaguesGroupedByCountry = () =>
-  fetch(`http://3.68.65.235:3000/odds/soccer/pregame/leagues/groupedBy/country`, requestOptions)
+  fetch(`http://${API_URL}/odds/soccer/pregame/leagues/groupedBy/country`, requestOptions)
     .then((response) => response.text())
     .then((result) => JSON.parse(result))
     .catch((error) => console.log("error", error));
