1d2e0223242dea5312d42b985e8a9202889fc1e6
Stratos Ntallaris <stratos@Stratos>
2023-10-12 06:04:59 +0300

    responsive improved in casino

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: {
         <html lang="en">
             <head>
 
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="viewport" content="width=device-width, user-scalable=no"></meta>
             </head>
             <body className="bg-[#282828]">
                 <Provider>
