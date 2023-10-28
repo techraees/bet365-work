91906fe0cf627610fa3c82bf81feb4c4a87c76ac
Stratos Ntallaris <stratos@Stratos>
2023-10-28 17:01:21 +0300

    betslip removed from main branch

--- src/app/(app)/in-play/[...sport]/page.tsx
+++ src/app/(app)/in-play/[...sport]/page.tsx
@@ -6,49 +6,54 @@ import requireSession from "@/lib/request-session";
 import SportsHeader from "../components/SportsHeader";
 
 const Home = async ({ params }: any) => {
-  const session = await requireSession();
-  let { sport } = params;
+	const session = await requireSession();
+	let { sport } = params;
 
-  let odds = await getSportsOdds(sport[0]);
-  console.log("in-play", sport[0], { odds });
-  if (sport[0] === "esports") {
-    console.log("calling other odds");
-    let soccerodds = await getSportsOdds("esoccer");
-    let basketballodds = await getSportsOdds("basketball");
-    odds = { ...odds, ...soccerodds, ...basketballodds };
-  }
+	let odds = await getSportsOdds(sport[0]);
+	console.log("in-play", sport[0], { odds });
+	if (sport[0] === "esports") {
+		console.log("calling other odds");
+		let soccerodds = await getSportsOdds("esoccer");
+		let basketballodds = await getSportsOdds("basketball");
+		odds = { ...odds, ...soccerodds, ...basketballodds };
+	}
 
-  // console.log({ odds, leagues });
+	// console.log({ odds, leagues });
 
-  const NoOddsFound = () => {
-    return (
-      <>
-        <SportsHeader />
-        <div className="pt-5 text-white flex items-center">No odds found</div>
-      </>
-    );
-  };
+	const NoOddsFound = () => {
+		return (
+			<>
+				<SportsHeader />
+				<div className="pt-5 text-white flex items-center">
+					No odds found
+				</div>
+			</>
+		);
+	};
 
-  if (odds === undefined || Object.keys(odds).length === 0) {
-    return <NoOddsFound />;
-  }
+	if (odds === undefined || Object.keys(odds).length === 0) {
+		return <NoOddsFound />;
+	}
 
-  if (odds?.message) {
-    return (
-      <div>
-        <SportsHeader />
-        <div className="pt-5 text-white flex items-center">No odds found</div>
-      </div>
-    );
-  }
-  return (
-    <Nats
-      odds={odds}
-      sport={sport[0]}
-      subcategory={sport[1]}
-      currentdataId={sport[2]}
-    />
-  );
+	if (odds?.message) {
+		return (
+			<div>
+				{" "}
+				<SportsHeader />
+				<div className="pt-5 text-white flex items-center">
+					No odds found
+				</div>
+			</div>
+		);
+	}
+	return (
+		<Nats
+			odds={odds}
+			sport={sport[0]}
+			subcategory={sport[1]}
+			currentdataId={sport[2]}
+		/>
+	);
 };
 
 export default Home;
