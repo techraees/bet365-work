83dc028a3971417c299eeaf71b192ab2abc71ed6
awesomedev08 <awesomedev08@gmail.com>
2024-01-12 23:56:26 -0800

    Line Break fixed

--- src/app/(app)/my-bets/components/Scroller/BetsContainer/BetItem/BetItemInnerView/BetViewDetails/BetParticipant/BetPartTopContainer/BetPartLeftContainer/BetPartFixtureContainer/BetPartFixtureContainer.tsx
+++ src/app/(app)/my-bets/components/Scroller/BetsContainer/BetItem/BetItemInnerView/BetViewDetails/BetParticipant/BetPartTopContainer/BetPartLeftContainer/BetPartFixtureContainer/BetPartFixtureContainer.tsx
@@ -10,17 +10,13 @@ export default function BetPartFixtureContainer({ matchName, score, playTime }: 
   return (
     <div className='flex'>
         <div className='font-normal text-xs leading-[15px] inline-block overflow-hidden leading-[15px] text-[11px] text-[#ddd] self-start cursor-pointer pl-0 pr-[5px] pt-0 pb-2.5 pb-[7px] pb-[3px]' style={{
-            display: "-webkit-box",
-            WebkitBoxOrient: "vertical",
-            lineClamp: "2",
-            WebkitLineClamp: "2",
             wordBreak: "break-word",
             wordWrap: "break-word"
         }}>
             <div className='inline-flex mr-[5px] hover:text-[color(display-p3_0.157_1_0.733)]'>{matchName}</div>
             <div className='inline text-[:#ddd]'>
-                <div className='inline-block bg-[hsla(0,0%,100%,0.1)] text-white font-bold mr-[5px] px-[3px] py-0 rounded-[1px]'>{score}</div>
-                <div className='inline-block'>{playTime}</div>
+                <div className='inline-block bg-[hsla(0,0%,100%,0.1)] text-white font-bold mr-[5px] px-[3px] py-0 rounded-[1px]'>{"3:0,1:2,4:3,7:9"}{score}</div>
+                <div className='inline-block'>{"31:42"}{playTime}</div>
             </div>
         </div>
     </div>
