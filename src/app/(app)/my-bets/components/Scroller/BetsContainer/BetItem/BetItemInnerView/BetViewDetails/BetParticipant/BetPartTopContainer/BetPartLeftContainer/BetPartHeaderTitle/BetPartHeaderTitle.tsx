bbb40f6db0cac1b0dde620421247cc66513d5270
Stratos Ntallaris <stratos@Stratos>
2024-02-20 18:16:36 +0200

    HALF LOST FIX

--- src/app/(app)/my-bets/components/Scroller/BetsContainer/BetItem/BetItemInnerView/BetViewDetails/BetParticipant/BetPartTopContainer/BetPartLeftContainer/BetPartHeaderTitle/BetPartHeaderTitle.tsx
+++ src/app/(app)/my-bets/components/Scroller/BetsContainer/BetItem/BetItemInnerView/BetViewDetails/BetParticipant/BetPartTopContainer/BetPartLeftContainer/BetPartHeaderTitle/BetPartHeaderTitle.tsx
@@ -17,7 +17,9 @@ export default function BetPartHeaderTitle({ team, handicap, status }: Props) {
           <div className='w-[13px] h-[13px] absolute bg-no-repeat bg-[50%] left-[-18px] top-1 after:content-[""] after:absolute after:w-[7px] after:h-[7px] after:bg-[#ddd] after:rounded-[50%] after:border-[none] after:right-[3px] after:top-[3px]'></div>
         </>
       )}
-      {(status === "WON" || status === "HALF WON") && (
+      {(status === "WON" ||
+        status === "HALF WON" ||
+        status === "HALF LOST") && (
         <>
           <div className="absolute w-[13px] h-[13px] absolute bg-no-repeat bg-[50%] left-[-20px] top-[3px]">
             <svg
@@ -40,7 +42,7 @@ export default function BetPartHeaderTitle({ team, handicap, status }: Props) {
           </div>
         </>
       )}
-      {(status === "LOST" || status === "HALF LOST") && (
+      {status === "LOST" && (
         <>
           <div className="absolute w-[13px] h-[13px] absolute bg-no-repeat bg-[50%] left-[-20px] top-[3px]">
             <svg
